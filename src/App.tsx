72cd9c63b0cb68b37b9e535f350e203d5811ed71
yoonjong.park <yoonjongpark@gmail.com>
2023-01-20 08:25:52 +0900

    CHANGE the position of dragHandleProps

--- src/App.tsx
+++ src/App.tsx
@@ -14,20 +14,16 @@ function App() {
             <ul ref={magic.innerRef} {...magic.droppableProps}>
               <Draggable draggableId="first" index={0}>
                 {magic => (
-                  <li
-                    ref={magic.innerRef}
-                    {...magic.draggableProps}
-                    {...magic.dragHandleProps}>
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    <span {...magic.dragHandleProps}>😆</span>
                     One
                   </li>
                 )}
               </Draggable>
               <Draggable draggableId="second" index={1}>
                 {magic => (
-                  <li
-                    ref={magic.innerRef}
-                    {...magic.draggableProps}
-                    {...magic.dragHandleProps}>
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    <span {...magic.dragHandleProps}>😆</span>
                     Two
                   </li>
                 )}
